c80cf982b62e3174f17d469025d044ef86765f86
Angelo0512 <murillocalvo342@gmail.com>
2022-04-18 22:15:28 -0600

    Testing Classes

---
+++ backend-app-bravo/src/test/kotlin/backend/workshop/LoadInitData.kt
@@ -0,0 +1,24 @@
+package backend.workshop
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.jdbc.Sql
+
+@SpringBootTest
+@Sql(/*"/import-users.sql", "/import-priorities.sql", "/import-status.sql", "/import-tasks.sql", "/import-reminders.sql"*/)
+/**
+ * This class will load the initial data into the database
+ */
+class LoadInitData (
+    //@Autowired
+    //val taskRepository: TaskRepository,
+) {
+
+    @Test
+    fun testTaskFindAll() {
+        //val taskList: List<Task> = taskRepository.findAll()
+        //Assertions.assertTrue(taskList.size == 2)
+    }
+}
