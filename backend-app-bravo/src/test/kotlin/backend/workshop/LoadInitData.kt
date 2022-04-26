8f6c6c4bec9b3e4065f20d2235c4858460b2308f
AraR11 <aramisramirez19@gmail.com>
2022-04-26 14:53:36 -0600

    db testing complete

--- backend-app-bravo/src/test/kotlin/backend/workshop/LoadInitData.kt
+++ backend-app-bravo/src/test/kotlin/backend/workshop/LoadInitData.kt
@@ -14,11 +14,39 @@ import org.springframework.test.context.jdbc.Sql
 class LoadInitData (
     @Autowired
     val userRepository: UserRepository,
-) {
+
+    @Autowired
+    val vehicleRepository: VehicleRepository,
+
+    @Autowired
+    val clientRepository: ClientRepository,
+
+    @Autowired
+    val serviceRepository: ServiceRepository,
+
+    @Autowired
+    val technicianRepository: TechnicianRepository,
+
+    @Autowired
+    val reportRepository: ReportRepository,
+
+    ) {
 
     @Test
-    fun testUserFindAll() {
+    fun testFindAll() {
         val userList: List<User> = userRepository.findAll()
-        Assertions.assertTrue(userList.size == 2)
+        val vehicleList: List<Vehicle> = vehicleRepository.findAll()
+        val clientList: List<Client> = clientRepository.findAll()
+        val serviceList: List<Service> = serviceRepository.findAll()
+        val technicianList: List<Technician> = technicianRepository.findAll()
+        val reportList: List<Report> = reportRepository.findAll()
+        Assertions.assertAll("Import database test",
+            { Assertions.assertTrue(userList.size == 2) },
+            { Assertions.assertTrue(vehicleList.size == 2) },
+            { Assertions.assertTrue(clientList.size == 3) },
+            { Assertions.assertTrue(serviceList.size == 3) },
+            { Assertions.assertTrue(technicianList.size == 1) },
+            { Assertions.assertTrue(reportList.size == 2) }
+        )
     }
 }
--- backend-app-bravo/src/test/resources/import-database.sql
+++ backend-app-bravo/src/test/resources/import-database.sql
@@ -14,7 +14,6 @@ INSERT INTO public.role_privilege (role_id, privilege_id) VALUES (1, 1);
 /** Users **/
 INSERT INTO public.users (id, create_date, email, enabled, first_name, last_name, password, token_expired)
 VALUES (1, '2020-04-18 22:26:00.000000', 'myemail@test.com', true, 'John', 'Doe', 'pass$123', false);
-
 INSERT INTO public.users (id, create_date, email, enabled, first_name, last_name, password, token_expired)
 VALUES (2, '2020-04-18 22:26:00.000000', 'jane@test.com', true, 'Jane', 'Doe', 'pass$456', false);
 
@@ -27,16 +26,43 @@ VALUES (1, 'Audi','A12BC','Motor electrico','A34589','Deportivo', '123');
 INSERT INTO public.vehicle (id, brand,motor_serial, motor_type,plate_number,vehicle_class,vin_number)
 VALUES (2, 'Toyota','A12BC','Motor diesel','A98543','Pickup', '124');
 
+
 /** Services **/
-INSERT INTO public.service (id, oberservations, state)
-VALUES (1, 'Cambio de aceite','ACTIVO');
-INSERT INTO public.service (id, oberservations, state)
-VALUES (2, 'Mantenimiento preventivo', 'COMPLETO');
-INSERT INTO public.service (id, oberservations, state)
-VALUES (3, 'Mantenimiento de neumaticos','ACTIVO');
+INSERT INTO public.service (id, observations, state)
+VALUES (1, 'Cambio de aceite',1);
+INSERT INTO public.service (id, observations, state)
+VALUES (2, 'Mantenimiento preventivo', 1);
+INSERT INTO public.service (id, observations, state)
+VALUES (3, 'Mantenimiento de neumaticos',1);
 
 /** Clients **/
-INSERT INTO public.service (id, ,email, name, telephone)
+INSERT INTO public.client (id ,email, name, telephone)
 VALUES (1, 'jane@test.com','Jane Doe', 61234567);
-INSERT INTO public.service (id, ,email, name, telephone)
+INSERT INTO public.client (id ,email, name, telephone)
 VALUES (2, 'bruce@test.com','Bruce Doe', 62234566);
+INSERT INTO public.client (id ,email, name, telephone)
+VALUES (3, 'dorothy@test.com','Dorothy Rowland', 62234555);
+
+
+/** client_vehicle **/
+INSERT INTO public.client_vehicle (client_id, vehicle_id) VALUES (1, 2);
+INSERT INTO public.client_vehicle (client_id, vehicle_id) VALUES (2, 1);
+INSERT INTO public.client_vehicle (client_id, vehicle_id) VALUES (3, 1);
+
+/** technicians **/
+INSERT INTO public.technician (id, user_id)
+VALUES (1, 1);
+
+/** reports **/
+INSERT INTO public.report (id, creation_date,description,client_id,technician_id,vehicle_id)
+VALUES (1, '2020-04-26 14:26:00.000000','Reparacion cambio de aceite y neumaticos',1,1,2);
+INSERT INTO public.report (id, creation_date,description,client_id,technician_id,vehicle_id)
+VALUES (2, '2020-04-28 11:45:00.000000','Mantenimiento preventivo de vehiculo',2,1,1);
+
+/**report_services**/
+INSERT INTO public.report_services (report_id, service_id)
+VALUES (1, 1);
+INSERT INTO public.report_services (report_id, service_id)
+VALUES (1, 3);
+INSERT INTO public.report_services (report_id, service_id)
+VALUES (2, 2);
