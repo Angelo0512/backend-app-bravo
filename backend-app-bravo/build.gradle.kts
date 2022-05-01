ed8fc2115e3c19b45809bee539a962f0cc6f5af9
Angelo0512 <murillocalvo342@gmail.com>
2022-04-30 19:03:24 -0600

    Update build.gradle.kts

--- backend-app-bravo/build.gradle.kts
+++ backend-app-bravo/build.gradle.kts
@@ -26,9 +26,12 @@ dependencies {
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation ("org.mapstruct:mapstruct:1.5.0.RC1")
+    annotationProcessor ("org.mapstruct:mapstruct:1.5.0.RC1")
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     kapt("org.springframework.boot:spring-boot-configuration-processor")
+
     runtimeOnly("org.postgresql:postgresql")
 }
 
