0f8cba32b687900498d462881ccf6adea7ffa1c2
Angelo0512 <murillocalvo342@gmail.com>
2022-04-30 19:59:35 -0600

    Vehicle and Client DTO

--- backend-app-bravo/src/main/kotlin/backend/workshop/DTOs.kt
+++ backend-app-bravo/src/main/kotlin/backend/workshop/DTOs.kt
@@ -39,3 +39,39 @@ data class UserResult(
     var createDate:Date,
     var roles:List<RoleDetails>,
 )
+
+data class VehicleInput(
+    var id:Long? = null,
+    var plateNumbre:String? = null,
+    var vinNumber:String? = null,
+    var brand:String? = null,
+    var motorSerial:String? = null,
+    var vechicleClass:String? = null,
+    var motorType:String? = null,
+)
+
+data class VehicleResult(
+    var id:Long,
+    var plateNumbre:String,
+    var vinNumber:String,
+    var brand:String,
+    var motorSerial:String,
+    var vechicleClass:String,
+    var motorType:String,
+)
+
+data class ClientInput(
+    var id:Long? = null,
+    var name:String? = null,
+    var email:String? = null,
+    var telephone:Int? = null,
+    var vehicles:List<VehicleInput>? = null,
+)
+
+data class ClientResult(
+    var id:Long,
+    var name:String,
+    var email:String,
+    var telephone:Int,
+    var vehicles:List<VehicleInput>,
+)
