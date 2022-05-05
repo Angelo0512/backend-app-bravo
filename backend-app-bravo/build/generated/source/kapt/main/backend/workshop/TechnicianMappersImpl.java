1ffa5f5e39c5a22cf5b8afe54beb990346f996e0
Haxiz <assassinsand@gmail.com>
2022-05-05 12:07:34 -0600

    start of web services

--- backend-app-bravo/build/generated/source/kapt/main/backend/workshop/TechnicianMappersImpl.java
+++ backend-app-bravo/build/generated/source/kapt/main/backend/workshop/TechnicianMappersImpl.java
@@ -5,14 +5,14 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-05-05T11:34:18-0600",
-    comments = "version: 1.5.0.RC1, compiler: IncrementalProcessingEnvironment from kotlin-annotation-processing-gradle-1.6.10.jar, environment: Java 11.0.15 (Amazon.com Inc.)"
+    date = "2022-05-05T12:06:57-0600",
+    comments = "version: 1.5.0.RC1, compiler: IncrementalProcessingEnvironment from kotlin-annotation-processing-gradle-1.6.10.jar, environment: Java 11.0.14.1 (Amazon.com Inc.)"
 )
 @Component
 public class TechnicianMappersImpl implements TechnicianMappers {
 
     @Override
-    public TechnicianDetails technicianToTechnicianDetails(Technician technician) {
+    public TechnicianDetails technicianToTechnicianDetails(User technician) {
         if ( technician == null ) {
             return null;
         }
@@ -20,25 +20,7 @@ public class TechnicianMappersImpl implements TechnicianMappers {
         TechnicianDetails technicianDetails = new TechnicianDetails();
 
         technicianDetails.setId( technician.getId() );
-        technicianDetails.setUser( userToUserInput( technician.getUser() ) );
 
         return technicianDetails;
-    }
-
-    protected UserInput userToUserInput(User user) {
-        if ( user == null ) {
-            return null;
-        }
-
-        UserInput userInput = new UserInput();
-
-        userInput.setId( user.getId() );
-        userInput.setFirstName( user.getFirstName() );
-        userInput.setLastName( user.getLastName() );
-        userInput.setEmail( user.getEmail() );
-        userInput.setPassword( user.getPassword() );
-        userInput.setEnabled( user.getEnabled() );
-
-        return userInput;
     }
 }
--- backend-app-bravo/build/tmp/kapt3/stubs/main/backend/workshop/AbstractVehicleService.java
+++ backend-app-bravo/build/tmp/kapt3/stubs/main/backend/workshop/AbstractVehicleService.java
@@ -2,7 +2,7 @@ package backend.workshop;
 
 import java.lang.System;
 
-@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000:\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\b\u0002\n\u0002\u0010\t\n\u0000\n\u0002\u0010\u000e\n\u0002\b\u0005\b\u0017\u0018\u00002\u00020\u0001B\u0019\u0012\b\b\u0001\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0001\u0010\u0004\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\u0006J\u0012\u0010\u000b\u001a\u0004\u0018\u00010\f2\u0006\u0010\r\u001a\u00020\u000eH\u0016J\u0010\u0010\u000f\u001a\n\u0012\u0004\u0012\u00020\f\u0018\u00010\u0010H\u0016J\u0012\u0010\u0011\u001a\u0004\u0018\u00010\f2\u0006\u0010\u0012\u001a\u00020\u0013H\u0016J\u0012\u0010\u0014\u001a\u0004\u0018\u00010\f2\u0006\u0010\u0012\u001a\u00020\u0015H\u0016J\u0012\u0010\u0016\u001a\u0004\u0018\u00010\f2\u0006\u0010\u0017\u001a\u00020\u0015H\u0016J\u0012\u0010\u0018\u001a\u0004\u0018\u00010\f2\u0006\u0010\u0019\u001a\u00020\u0015H\u0016R\u0014\u0010\u0004\u001a\u00020\u0005X\u0096\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\bR\u0014\u0010\u0002\u001a\u00020\u0003X\u0096\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\n\u00a8\u0006\u001a"}, d2 = {"Lbackend/workshop/AbstractVehicleService;", "Lbackend/workshop/VehicleService;", "vehicleRepository", "Lbackend/workshop/VehicleRepository;", "vehicleMapper", "Lbackend/workshop/VehicleMapper;", "(Lbackend/workshop/VehicleRepository;Lbackend/workshop/VehicleMapper;)V", "getVehicleMapper", "()Lbackend/workshop/VehicleMapper;", "getVehicleRepository", "()Lbackend/workshop/VehicleRepository;", "create", "Lbackend/workshop/VehicleResult;", "vehicleInput", "Lbackend/workshop/VehicleInput;", "findAll", "", "findById", "id", "", "findByOwner", "", "findByPlateNumber", "plateNumber", "findByVinNumber", "vinNumber", "backend-app-bravo"})
+@kotlin.Metadata(mv = {1, 6, 0}, k = 1, d1 = {"\u0000<\n\u0002\u0018\u0002\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0002\b\u0006\n\u0002\u0018\u0002\n\u0000\n\u0002\u0018\u0002\n\u0000\n\u0002\u0010 \n\u0002\b\u0002\n\u0002\u0010\t\n\u0002\b\u0002\n\u0002\u0010\u000e\n\u0002\b\u0003\b\u0017\u0018\u00002\u00020\u0001B\u0019\u0012\b\b\u0001\u0010\u0002\u001a\u00020\u0003\u0012\b\b\u0001\u0010\u0004\u001a\u00020\u0005\u00a2\u0006\u0002\u0010\u0006J\u0012\u0010\u000b\u001a\u0004\u0018\u00010\f2\u0006\u0010\r\u001a\u00020\u000eH\u0016J\u0010\u0010\u000f\u001a\n\u0012\u0004\u0012\u00020\f\u0018\u00010\u0010H\u0016J\u0012\u0010\u0011\u001a\u0004\u0018\u00010\f2\u0006\u0010\u0012\u001a\u00020\u0013H\u0016J\u0012\u0010\u0014\u001a\u0004\u0018\u00010\f2\u0006\u0010\u0015\u001a\u00020\u0016H\u0016J\u0012\u0010\u0017\u001a\u0004\u0018\u00010\f2\u0006\u0010\u0018\u001a\u00020\u0016H\u0016R\u0014\u0010\u0004\u001a\u00020\u0005X\u0096\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\u0007\u0010\bR\u0014\u0010\u0002\u001a\u00020\u0003X\u0096\u0004\u00a2\u0006\b\n\u0000\u001a\u0004\b\t\u0010\n\u00a8\u0006\u0019"}, d2 = {"Lbackend/workshop/AbstractVehicleService;", "Lbackend/workshop/VehicleService;", "vehicleRepository", "Lbackend/workshop/VehicleRepository;", "vehicleMapper", "Lbackend/workshop/VehicleMapper;", "(Lbackend/workshop/VehicleRepository;Lbackend/workshop/VehicleMapper;)V", "getVehicleMapper", "()Lbackend/workshop/VehicleMapper;", "getVehicleRepository", "()Lbackend/workshop/VehicleRepository;", "create", "Lbackend/workshop/VehicleResult;", "vehicleInput", "Lbackend/workshop/VehicleInput;", "findAll", "", "findById", "id", "", "findByPlateNumber", "plateNumber", "", "findByVinNumber", "vinNumber", "backend-app-bravo"})
 @org.springframework.stereotype.Service()
 public class AbstractVehicleService implements backend.workshop.VehicleService {
     @org.jetbrains.annotations.NotNull()
@@ -54,13 +54,6 @@ public class AbstractVehicleService implements backend.workshop.VehicleService {
     @java.lang.Override()
     public backend.workshop.VehicleResult findByVinNumber(@org.jetbrains.annotations.NotNull()
     java.lang.String vinNumber) throws java.util.NoSuchElementException {
-        return null;
-    }
-    
-    @org.jetbrains.annotations.Nullable()
-    @java.lang.Override()
-    public backend.workshop.VehicleResult findByOwner(@org.jetbrains.annotations.NotNull()
-    java.lang.String id) {
         return null;
     }
     
