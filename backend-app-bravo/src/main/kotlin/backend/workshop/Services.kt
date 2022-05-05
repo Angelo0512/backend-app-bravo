e71c509dd8e0f9db09c52cabaeebc9cc6af9dd5f
Angelo0512 <murillocalvo342@gmail.com>
2022-05-05 17:03:14 -0600

    New Service added AppUserDetailsService

--- backend-app-bravo/src/main/kotlin/backend/workshop/Services.kt
+++ backend-app-bravo/src/main/kotlin/backend/workshop/Services.kt
@@ -260,3 +260,62 @@ class AbstractReportService(
         )
     }
 }
+
+@Service
+@Transactional
+class AppUserDetailsService(
+    @Autowired
+    val userRepository: UserRepository,
+    @Autowired
+    val roleRepository: RoleRepository,
+    @Autowired
+    val roleMapper: RoleMapper,
+) : UserDetailsService {
+
+    @Throws(UsernameNotFoundException::class)
+    override fun loadUserByUsername(username: String): UserDetails {
+        var userAuth: org.springframework.security.core.userdetails.User? = null
+        val user: Optional<User> = userRepository.findByEmail(username)
+        if (user.isEmpty) {
+            return org.springframework.security.core.userdetails.User(
+                "", "", true, true, true, true,
+                getAuthorities(Arrays.asList(
+                    roleRepository.findByName("ROLE_USER").get())))
+        }
+
+        userAuth = org.springframework.security.core.userdetails.User(
+            user.get().email, user.get().password, user.get().enabled, true, true,
+            true, getAuthorities(user.get().roleList!!.toMutableList()))
+
+        return userAuth
+    }
+
+    private fun getAuthorities(
+        roles: MutableList<Role>,
+    ): Collection<GrantedAuthority?>? {
+        return getGrantedAuthorities(getPrivileges(roles))
+    }
+
+    private fun getPrivileges(roles: MutableList<Role>?): List<String> {
+        val privileges: MutableList<String> = ArrayList()
+        val collection: MutableList<Privilege> = ArrayList()
+        if (roles != null) {
+            for (role in roles) {
+                collection.addAll(role.privilegeList)
+            }
+        }
+        for (item in collection) {
+            privileges.add(item.name)
+        }
+        return privileges
+    }
+
+    private fun getGrantedAuthorities(privileges: List<String>): List<GrantedAuthority?>? {
+        val authorities: MutableList<GrantedAuthority?> = ArrayList()
+        for (privilege in privileges) {
+            authorities.add(SimpleGrantedAuthority(privilege))
+        }
+        return authorities
+    }
+
+}
