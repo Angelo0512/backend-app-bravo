5c733e7fc295a9028cc75262242b15fc05dc6726
ArnoldG6 <arnoldgq612@gmail.com>
2022-05-05 10:02:39 -0600

    .

--- backend-app-bravo/src/main/kotlin/backend/workshop/Repositories.kt
+++ backend-app-bravo/src/main/kotlin/backend/workshop/Repositories.kt
@@ -2,6 +2,8 @@ package backend.workshop
 
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.data.jpa.repository.Query
+import org.springframework.data.repository.query.Param
 
 @Repository
 interface ClientRepository: JpaRepository<Client, Long>
