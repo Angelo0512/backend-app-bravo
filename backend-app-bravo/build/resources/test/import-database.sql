1b605a071726841902ff790617dfd24f419e945a
AraR11 <aramisramirez19@gmail.com>
2022-05-05 13:49:30 -0600

    add test users

--- backend-app-bravo/build/resources/test/import-database.sql
+++ backend-app-bravo/build/resources/test/import-database.sql
@@ -1,68 +1,51 @@
 
 
-/** Privilege **/
+/** Privilege**/
 INSERT INTO public.privilege (id, name) VALUES (1, 'TECHNICIAN_PRIVILEGE');
 INSERT INTO public.privilege (id, name) VALUES (2, 'CLIENT_PRIVILEGE');
 
-/** Role **/
+/** Role**/
 INSERT INTO public.role (id, name) VALUES (1, 'ROLE_TECH');
 INSERT INTO public.role (id, name) VALUES (2, 'ROLE_CLI');
 
-/** Role-Privilege **/
-INSERT INTO public.role_privilege (role_id, privilege_id) VALUES (1, 1);
+/** Role-Privilege**/
+INSERT INTO public.role_privilege (role_id, privilege_id) VALUES (2, 1);
 
 /** Users **/
 INSERT INTO public.users (id, create_date, email, enabled, first_name, last_name, password, token_expired)
-VALUES (1, '2020-04-18 22:26:00.000000', 'myemail@test.com', true, 'John', 'Doe', 'pass$123', false);
+VALUES (3, '2020-04-18 22:26:00.000000', 'bruce@test.com', true, 'Bruce', 'Doe', 'pass$111', false);
 INSERT INTO public.users (id, create_date, email, enabled, first_name, last_name, password, token_expired)
-VALUES (2, '2020-04-18 22:26:00.000000', 'jane@test.com', true, 'Jane', 'Doe', 'pass$456', false);
+VALUES (4, '2020-04-18 22:26:00.000000', 'jade@test.com', true, 'Jade', 'Doe', 'pass$444', false);
 
 /** User-Role **/
-INSERT INTO public.user_role (user_id, role_id) VALUES (1, 1);
+INSERT INTO public.user_role (user_id, role_id) VALUES (3, 1);
 
-/** Vehicles **/
+/** Vehicles
 INSERT INTO public.vehicle (id, brand,motor_serial, motor_type,plate_number,vehicle_class,vin_number)
 VALUES (1, 'Audi','A12BC','Motor electrico','A34589','Deportivo', '123');
 INSERT INTO public.vehicle (id, brand,motor_serial, motor_type,plate_number,vehicle_class,vin_number)
-VALUES (2, 'Toyota','A12BC','Motor diesel','A98543','Pickup', '124');
+VALUES (2, 'Toyota','A12BC','Motor diesel','A98543','Pickup', '124');**/
 
 
-/** Services **/
+/** Services
 INSERT INTO public.service (id, observations, state)
 VALUES (1, 'Cambio de aceite',1);
 INSERT INTO public.service (id, observations, state)
 VALUES (2, 'Mantenimiento preventivo', 1);
 INSERT INTO public.service (id, observations, state)
-VALUES (3, 'Mantenimiento de neumaticos',1);
-
-/** Clients **/
-INSERT INTO public.client (id ,email, name, telephone)
-VALUES (1, 'jane@test.com','Jane Doe', 61234567);
-INSERT INTO public.client (id ,email, name, telephone)
-VALUES (2, 'bruce@test.com','Bruce Doe', 62234566);
-INSERT INTO public.client (id ,email, name, telephone)
-VALUES (3, 'dorothy@test.com','Dorothy Rowland', 62234555);
+VALUES (3, 'Mantenimiento de neumaticos',1);**/
 
 
-/** client_vehicle **/
-INSERT INTO public.client_vehicle (client_id, vehicle_id) VALUES (1, 2);
-INSERT INTO public.client_vehicle (client_id, vehicle_id) VALUES (2, 1);
-INSERT INTO public.client_vehicle (client_id, vehicle_id) VALUES (3, 1);
-
-/** technicians **/
-INSERT INTO public.technician (id, user_id)
-VALUES (1, 1);
-
-/** reports **/
-INSERT INTO public.report (id, creation_date,description,client_id,technician_id,vehicle_id)
+/** reports
+INSERT INTO public.report (id, creation_date,description,client,technician,vehicle_id)
 VALUES (1, '2020-04-26 14:26:00.000000','Reparacion cambio de aceite y neumaticos',1,1,2);
 INSERT INTO public.report (id, creation_date,description,client_id,technician_id,vehicle_id)
-VALUES (2, '2020-04-28 11:45:00.000000','Mantenimiento preventivo de vehiculo',2,1,1);
+VALUES (2, '2020-04-28 11:45:00.000000','Mantenimiento preventivo de vehiculo',2,1,1);**/
 
-/**report_services**/
+/**report_services
 INSERT INTO public.report_services (report_id, service_id)
 VALUES (1, 1);
 INSERT INTO public.report_services (report_id, service_id)
 VALUES (1, 3);
 INSERT INTO public.report_services (report_id, service_id)
-VALUES (2, 2);
+VALUES (2, 2);**/
