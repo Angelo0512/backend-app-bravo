36d7b9988e8ed1f7f0718f21a297c842f02ae234
ArnoldG6 <arnoldgq612@gmail.com>
2022-05-05 10:01:02 -0600

    adding find by methods in vehicle rep

--- backend-app-bravo/src/main/kotlin/backend/workshop/Repositories.kt
+++ backend-app-bravo/src/main/kotlin/backend/workshop/Repositories.kt
@@ -25,4 +25,7 @@ interface TechnicianRepository: JpaRepository<Technician, Long>
 interface UserRepository: JpaRepository<User, Long>
 
 @Repository
-interface VehicleRepository: JpaRepository<Vehicle, Long>
+interface VehicleRepository: JpaRepository<Vehicle, Long>{
+    fun findByPlateNumber(@Param("plateNumber") plateNumber : String)
+    fun findByVinNumber(@Param("vinNumber") vinNumber : String)
+}
