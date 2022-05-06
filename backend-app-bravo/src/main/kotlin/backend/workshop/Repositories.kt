b611b5fd8832cd0db3c8308af17fb1dfb0fbf042
AraR11 <aramisramirez19@gmail.com>
2022-05-05 18:13:17 -0600

    add dependency

--- backend-app-bravo/src/main/kotlin/backend/workshop/Repositories.kt
+++ backend-app-bravo/src/main/kotlin/backend/workshop/Repositories.kt
@@ -4,6 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
+import java.util.*
 
 @Repository
 interface ClientRepository: JpaRepository<User, Long>
