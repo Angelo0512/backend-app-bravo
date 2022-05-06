52ee9db6406abf11397bf3179fb4dfec09b26733
Haxiz <assassinsand@gmail.com>
2022-05-05 23:23:57 -0600

    change urls to use query params instead of path params

--- backend-app-bravo/src/main/kotlin/backend/workshop/Webservices.kt
+++ backend-app-bravo/src/main/kotlin/backend/workshop/Webservices.kt
@@ -6,8 +6,10 @@ import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.ResponseBody
 import org.springframework.web.bind.annotation.RestController
+
 
 @RestController
 @RequestMapping("\${url.vehicles}")
@@ -15,19 +17,22 @@ class VehicleController(private val vehicleService: VehicleService) {
 
     @GetMapping
     @ResponseBody
-    fun findAll() = vehicleService.findAll()
-
-    @GetMapping("/id/{id}")
-    @ResponseBody
-    fun findById(@PathVariable id: Long) = vehicleService.findById(id)
-
-    @GetMapping("/plate/{plate}")
-    @ResponseBody
-    fun findByPlateNumber(@PathVariable plate: String) = vehicleService.findByPlateNumber(plate)
-
-    @GetMapping("/vin/{vin}")
-    @ResponseBody
-    fun findByVinNumber(@PathVariable vin: String) = vehicleService.findByVinNumber(vin)
+    @Suppress("IMPLICIT_CAST_TO_ANY")
+    fun find(@RequestParam(required = false) param: Map<String, String>) =
+        when {
+            param.containsKey("id") -> {
+                param["id"]?.let { vehicleService.findById(it.toLong()) }
+            }
+            param.containsKey("plate") -> {
+                param["plate"]?.let { vehicleService.findByPlateNumber(it) }
+            }
+            param.containsKey("vin") -> {
+                param["vin"]?.let { vehicleService.findByVinNumber(it) }
+            }
+            else -> {
+                vehicleService.findAll()
+            }
+        }
 
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
@@ -42,15 +47,19 @@ class ServiceController(private val serviceService: ServiceService) {
 
     @GetMapping
     @ResponseBody
-    fun findAll() = serviceService.findAll()
-
-    @GetMapping("/id/{id}")
-    @ResponseBody
-    fun findById(@PathVariable id: Long) = serviceService.findById(id)
-
-    @GetMapping("/state/{state}")
-    @ResponseBody
-    fun findByState(@PathVariable state: String) = serviceService.findByState(state)
+    @Suppress("IMPLICIT_CAST_TO_ANY")
+    fun find(@RequestParam(required = false) param: Map<String, String>) =
+        when {
+            param.containsKey("id") -> {
+                param["id"]?.let { serviceService.findById(it.toLong()) }
+            }
+            param.containsKey("state") -> {
+                param["state"]?.let { serviceService.findByState(it) }
+            }
+            else -> {
+                serviceService.findAll()
+            }
+        }
 
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
@@ -65,19 +74,22 @@ class ReportController(private val reportService: ReportService) {
 
     @GetMapping
     @ResponseBody
-    fun findAll() = reportService.findAll()
-
-    @GetMapping("/id/{id}")
-    @ResponseBody
-    fun findById(@PathVariable id: Long) = reportService.findById(id)
-
-    @GetMapping("/client_id/{client_id}")
-    @ResponseBody
-    fun findByClientId(@PathVariable client_id: Long) = reportService.findByClientId(client_id)
-
-    @GetMapping("/plate/{plate}")
-    @ResponseBody
-    fun findByVehiclePlateNumber(@PathVariable plate: String) = reportService.findByVehicleNumberPlate(plate)
+    @Suppress("IMPLICIT_CAST_TO_ANY")
+    fun find(@RequestParam(required = false) param: Map<String, String>) =
+        when {
+            param.containsKey("id") -> {
+                param["id"]?.let { reportService.findById(it.toLong()) }
+            }
+            param.containsKey("client_id") -> {
+                param["client_id"]?.let { reportService.findByClientId(it.toLong()) }
+            }
+            param.containsKey("plate") -> {
+                param["plate"]?.let { reportService.findByVehicleNumberPlate(it) }
+            }
+            else -> {
+                reportService.findAll()
+            }
+        }
 
     @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
